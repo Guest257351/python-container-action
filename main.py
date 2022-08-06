566fd96712818709b4db3432faecab3cc4144180
Guest257351 <57662212+Guest257351@users.noreply.github.com>
2022-08-06 17:25:43 +0930

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ def main():
 {messages}
 ```
 """
+    json = {
+      "content": changelog,
+      "embeds": null,
+      "attachments": []
+      }
 
-    requests.post(
+    requests.post("https://discord.com/api/webhooks/1005383165013135420/Y3AqDuwTIMTC3hdAN2nl9ZK0QAyCFGwsuX-UQyShh0TWaCabsG8C7MeSnKwBP8G6A-5n", json=changelog
 
 
 if __name__ == "__main__":
