047a471aed8b71f71ba8675ca6c09b51a2e0c314
Guest257351 <57662212+Guest257351@users.noreply.github.com>
2022-08-06 17:31:06 +0930

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import requests
 
 
 def main():
-    commits = os.environ["commits"]
+    commits = os.environ["GITHUB_EVENT_COMMITS"]
 
-    version = os.environ["versionNo"]
+    version = os.environ["GITHUB_RUN_ID"]
 
     messages = '\n'.join([i.message for i in commits])
 
@@ -20,7 +20,7 @@ def main():
       "attachments": []
       }
 
-    requests.post("https://discord.com/api/webhooks/1005383165013135420/Y3AqDuwTIMTC3hdAN2nl9ZK0QAyCFGwsuX-UQyShh0TWaCabsG8C7MeSnKwBP8G6A-5n", json=changelog
+    requests.post("https://discord.com/api/webhooks/1005383165013135420/Y3AqDuwTIMTC3hdAN2nl9ZK0QAyCFGwsuX-UQyShh0TWaCabsG8C7MeSnKwBP8G6A-5n", json=json
 
 
 if __name__ == "__main__":
