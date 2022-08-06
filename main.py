d5afbddb44c17460f33c8748f9297ec26af3c22a
Guest257351 <57662212+Guest257351@users.noreply.github.com>
2022-08-06 17:22:02 +0930

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import requests
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    commits = os.environ["commits"]
 
-    for name, value in os.environ.items():
-        print("{0}: {1}".format(name, value))
-    my_output = f"Hello {my_input}"
+    version = os.environ["versionNo"]
 
-    print(f"::set-output name=myOutput::{my_output}")
+    messages = '\n'.join([i.message for i in commits])
+
+    changelog = f"""Version {version}
+```md
+{messages}
+```
+"""
+
+    requests.post(
 
 
 if __name__ == "__main__":
